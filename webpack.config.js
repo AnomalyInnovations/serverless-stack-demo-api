d8ad717b757f3d79c0957a092429fa1d051b253e
Fanjie Wang <frank@anomalyinnovations.com>
2017-02-28 15:23:53 -0500

    add-support-for-es6-javascript

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var nodeExternals = require('webpack-node-externals');
+var path = require('path');
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  entry: {
+  },
+  target: 'node',
+  // because 'aws-sdk' is not compatible with webpack,
+  // we exclude all node dependencies
+  externals: [nodeExternals()],
+  // run babel on all .js files and skip those in node_modules
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: __dirname,
+      exclude: /node_modules/,
+    }]
+  },
+  // since we are going to create multiple APIs in this guide, and we are 
+  // going to create a js file to for each, we need this output block
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+};
