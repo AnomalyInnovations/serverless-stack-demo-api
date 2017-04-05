d72525909c3cc253fc5dcbf577a2a111707b7116
Jay <jvelayud@gmail.com>
2017-04-05 19:02:46 -0400

    Updating to AWS promises

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -5,14 +5,5 @@ AWS.config.update({region:'us-east-1'});
 export function call(action, params) {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  return new Promise((resolve, reject) => {
-    dynamoDb[action](params, (error, result) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-
-      resolve(result);
-    });
-  });
+  return dynamoDb[action](params).promise();
 }
