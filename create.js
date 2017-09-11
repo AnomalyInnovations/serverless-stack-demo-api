8572d1cbb76042c62097bc0be02c5f30f241a111
Jay <jvelayud@gmail.com>
2017-09-10 20:23:34 -0400

    Updating

---
+++ create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "notes",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: new Date().getTime()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
