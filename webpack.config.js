7a3734c982a34016ce77c5dce5c9587b5f1687cf
Frank Schmid <HyperBrain@users.noreply.github.com>
2018-03-21 20:28:20 +0100

    Upgrade to Webpack 4 (#12)
    * Upgrade to Webpack 4
    
    * Removed temporary local serverless installation
    
    * Disable uglify and chunk size warnings

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
   // Since 'aws-sdk' is not compatible with webpack,
   // we exclude all node dependencies
   externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false
+  },
   // Run babel on all .js files and skip those in node_modules
   module: {
     rules: [
