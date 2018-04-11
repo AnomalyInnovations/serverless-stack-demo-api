5beca30a2789ea7af357adb23fdce0d5515c4ccf
Jay <jvelayud@gmail.com>
2018-04-11 17:13:23 -0400

    Updating to use starter

---
+++ list.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: "notes",
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    callback(null, success(result.Items));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
