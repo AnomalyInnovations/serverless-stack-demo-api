52127e7d282b9f11533ed8659bed7971c2a81b3c
Jay <jvelayud@gmail.com>
2018-04-11 17:10:57 -0400

    Updating to use starter

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,9 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "us-east-1" });
+
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
