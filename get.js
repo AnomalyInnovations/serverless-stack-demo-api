da90d8f75c8f6986a58032da7f5977f019bb0924
Jay <jvelayud@gmail.com>
2018-10-31 17:04:00 -0400

    Returning for async functions

--- get.js
+++ get.js
@@ -1,7 +1,7 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context, callback) {
+export async function main(event, context) {
   const params = {
     TableName: "notes",
     // 'Key' defines the partition key and sort key of the item to be retrieved
@@ -17,11 +17,11 @@ export async function main(event, context, callback) {
     const result = await dynamoDbLib.call("get", params);
     if (result.Item) {
       // Return the retrieved item
-      callback(null, success(result.Item));
+      return success(result.Item);
     } else {
-      callback(null, failure({ status: false, error: "Item not found." }));
+      return failure({ status: false, error: "Item not found." });
     }
   } catch (e) {
-    callback(null, failure({ status: false }));
+    return failure({ status: false });
   }
 }
