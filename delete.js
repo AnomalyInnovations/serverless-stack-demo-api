db644a8fe30d60c4afb91c61e0e15334746b8a77
Jay <jvelayud@gmail.com>
2018-10-31 17:09:00 -0400

    Returning for async functions

--- delete.js
+++ delete.js
@@ -1,7 +1,7 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context, callback) {
+export async function main(event, context) {
   const params = {
     TableName: "notes",
     // 'Key' defines the partition key and sort key of the item to be removed
@@ -15,8 +15,8 @@ export async function main(event, context, callback) {
 
   try {
     const result = await dynamoDbLib.call("delete", params);
-    callback(null, success({ status: true }));
+    return success({ status: true });
   } catch (e) {
-    callback(null, failure({ status: false }));
+    return failure({ status: false });
   }
 }
