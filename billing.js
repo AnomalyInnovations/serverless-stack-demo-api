4b44407226eeaac8fe4768f0bc87638ffcfdacc4
Jay <jvelayud@gmail.com>
2018-10-31 17:11:37 -0400

    Returning for async functions

--- billing.js
+++ billing.js
@@ -2,7 +2,7 @@ import stripePackage from "stripe";
 import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context, callback) {
+export async function main(event, context) {
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
   const description = "Scratch charge";
@@ -17,8 +17,8 @@ export async function main(event, context, callback) {
       description,
       currency: "usd"
     });
-    callback(null, success({ status: true }));
+    return success({ status: true });
   } catch (e) {
-    callback(null, failure({ message: e.message }));
+    return failure({ message: e.message });
   }
 }
