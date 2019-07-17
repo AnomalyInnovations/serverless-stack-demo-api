ea374bafa3e5957784dd08d7136bf54e4432593b
Jay <jvelayud@gmail.com>
2019-07-17 19:06:34 -0400

    Updating to serverless-bundle

---
+++ create.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "notes",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,7 @@
+import AWS from "aws-sdk";
+
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
