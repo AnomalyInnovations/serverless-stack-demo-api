eba78aa7292e444baec3c585dd229861d15ed1e0
Jay <jvelayud@gmail.com>
2019-08-03 20:44:19 -0400

    Moving faulty code inside function

--- create.js
+++ create.js
@@ -2,10 +2,11 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
+export async function main(event, context) {
+
 const a = 1;
 a.d.c;
 
-export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
