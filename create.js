40c0f1e65edcf4d5bb6bf87aa425834ca031d171
Jay <jvelayud@gmail.com>
2019-08-03 20:51:34 -0400

    Removing faulty code

--- create.js
+++ create.js
@@ -3,9 +3,6 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-
-uuid.abc.d;
-
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
