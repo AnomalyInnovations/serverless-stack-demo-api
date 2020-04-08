6b0e70e48794032ed5fc291a744f0f9da6a6f8e2
Frank <wangfanjie@gmail.com>
2020-04-07 22:55:32 -0400

    Adding error logging

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,12 +1,18 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
   return function (event, context) {
     return Promise.resolve()
+      // Start debugger
+      .then(() => debug.init(event, context))
       // Run the Lambda
       .then(() => lambda(event, context))
       // On success
       .then((responseBody) => [200, responseBody])
       // On failure
       .catch((e) => {
+        // Print debug messages
+        debug.flush(e);
         return [500, { error: e.message }];
       })
       // Return HTTP response
@@ -17,6 +23,8 @@ export default function handler(lambda) {
           "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(body),
-      }));
+      }))
+      // Cleanup debugger
+      .finally(debug.end);
   };
 }
