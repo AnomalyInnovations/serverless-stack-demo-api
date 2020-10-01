c1b18e323c1d1498ca64ee966fbaf91b30c1fb52
Jay <jvelayud@gmail.com>
2020-10-01 15:01:06 -0400

    Working through the steps

--- infrastructure/lib/S3Stack.js
+++ infrastructure/lib/S3Stack.js
@@ -3,6 +3,8 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as sst from "@serverless-stack/resources";
 
 export default class S3Stack extends sst.Stack {
+  bucket;
+
   constructor(scope, id, props) {
     super(scope, id, props);
 
--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,5 +1,5 @@
 import S3Stack from "./S3Stack";
-import CognitoStack from "./CognitoStack";
+//import CognitoStack from "./CognitoStack";
 import DynamoDBStack from "./DynamoDBStack";
 
 // Add stacks
@@ -7,6 +7,6 @@ export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
 
   const s3 = new S3Stack(app, "s3");
-
-  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
+  //
+  //  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
 }
