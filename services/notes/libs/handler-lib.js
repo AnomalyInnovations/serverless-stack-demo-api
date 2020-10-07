412fe519bc509344cad896c1703cf712d6ef9d1b
Frank <wangfanjie@gmail.com>
2020-10-07 01:04:43 -0400

    Adding error logging

--- services/notes/libs/handler-lib.js
+++ services/notes/libs/handler-lib.js
@@ -1,12 +1,20 @@
+import * as debug from "./debug-lib";
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
+
+    // Start debugger
+    debug.init(event, context);
 
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
+      // Print debug messages
+      debug.flush(e);
+
       body = { error: e.message };
       statusCode = 500;
     }
