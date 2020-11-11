ff86a5cac6e8262ec69235a53a1cab1a7c5376c4
Jay <jvelayud@gmail.com>
2020-11-11 17:18:12 -0500

    Small tweaks

--- delete.js
+++ delete.js
@@ -5,12 +5,10 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be removed
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
-    }
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
+    },
   };
 
   await dynamoDb.delete(params);
