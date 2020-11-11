f4d8694eb7213e8058e4507fa6bae3a4a167793f
Jay <jvelayud@gmail.com>
2020-11-11 17:35:45 -0500

    Small tweaks

--- services/notes/create.js
+++ services/notes/create.js
@@ -6,21 +6,14 @@ export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    // 'Item' contains the attributes of the item to be created
-    // - 'userId': user identities are federated through the
-    //             Cognito Identity Pool, we will use the identity id
-    //             as the user id of the authenticated user
-    // - 'noteId': a unique uuid
-    // - 'content': parsed from request body
-    // - 'attachment': parsed from request body
-    // - 'createdAt': current Unix timestamp
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
-    }
+      // The attributes of the item to be created
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
   };
 
   await dynamoDb.put(params);
