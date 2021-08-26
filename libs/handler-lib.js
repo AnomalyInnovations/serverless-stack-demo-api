f64ad66c89c3fd53b75cfbcead561ed21d1a6d2c
Jay <jvelayud@gmail.com>
2021-08-25 20:37:15 -0400

    Update handler-lib.js

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,30 +1,32 @@
 import * as debug from "./debug-lib";
 
 export default function handler(lambda) {
-  return function (event, context) {
-    return Promise.resolve()
-      // Start debugger
-      .then(() => debug.init(event, context))
+  return async function (event, context) {
+    let body, statusCode;
+
+    // Start debugger
+    debug.init(event, context);
+
+    try {
       // Run the Lambda
-      .then(() => lambda(event, context))
-      // On success
-      .then((responseBody) => [200, responseBody])
-      // On failure
-      .catch((e) => {
-        // Print debug messages
-        debug.flush(e);
-        return [500, { error: e.message }];
-      })
-      // Return HTTP response
-      .then(([statusCode, body]) => ({
-        statusCode,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
-        body: JSON.stringify(body),
-      }))
-      // Cleanup debugger
-      .finally(debug.end);
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      // Print debug messages
+      debug.flush(e);
+
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+    };
   };
 }
