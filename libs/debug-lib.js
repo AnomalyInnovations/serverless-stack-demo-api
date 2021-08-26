1bc83be0a16892a6ee03a6a25ab20a4001fe700b
Jay <jvelayud@gmail.com>
2021-08-25 20:38:01 -0400

    Update debug-lib.js

--- libs/debug-lib.js
+++ libs/debug-lib.js
@@ -1,11 +1,17 @@
-import AWS from "aws-sdk";
 import util from "util";
+import AWS from "aws-sdk";
+
+let logs;
 
 // Log AWS SDK calls
 AWS.config.logger = { log: debug };
 
-let logs;
-let timeoutTimer;
+export default function debug() {
+  logs.push({
+    date: new Date(),
+    string: util.format.apply(null, arguments),
+  });
+}
 
 export function init(event, context) {
   logs = [];
@@ -16,27 +22,9 @@ export function init(event, context) {
     pathParameters: event.pathParameters,
     queryStringParameters: event.queryStringParameters,
   });
-
-  // Start timeout timer
-  timeoutTimer = setTimeout(() => {
-    timeoutTimer && flush(new Error("Lambda will timeout in 100 ms"));
-  }, context.getRemainingTimeInMillis() - 100);
-}
-
-export function end() {
-  // Clear timeout timer
-  clearTimeout(timeoutTimer);
-  timeoutTimer = null;
 }
 
 export function flush(e) {
   logs.forEach(({ date, string }) => console.debug(date, string));
   console.error(e);
-}
-
-export default function debug() {
-  logs.push({
-    date: new Date(),
-    string: util.format.apply(null, arguments),
-  });
 }
